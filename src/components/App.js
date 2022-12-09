e9025ce4d4e5ab2def0883acfc61a3d84ae2b6ab
ali-gharebaghi <ali.dev720@gmail.com>
2022-12-09 19:10:30 +0330

    create app-base

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <div >
+      <CssBaseline/>
+      <h1>Welcom Guys !!!
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
